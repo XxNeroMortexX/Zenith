27ff7643072600d1fe3ee8eef7a81dd0a161364a
XxNeroMortexX <XxNeroMortexX@gmail.com>
2025-02-17 14:58:53 -0700

    AutoCommit: Mon [ 02/17/2025 Time: 14:58:53 ]

--- init.lua
+++ init.lua
@@ -195,6 +195,7 @@ local spellNames = {
 
 -- Memorize each spell in the corresponding gem slot
 for slot, spellName in ipairs(spellNames) do
+	print("Checking %s in slot %d", spellName, slot)
     memorizeSpellInSlotIfNeeded(spellName, slot)
 end
 
