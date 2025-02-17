c95628241e2061b21b77b04f0001867c3738f88c
XxNeroMortexX <XxNeroMortexX@gmail.com>
2025-02-17 14:39:28 -0700

    AutoCommit: Mon [ 02/17/2025 Time: 14:39:28 ]

--- init.lua
+++ init.lua
@@ -170,7 +170,8 @@ end
 local function memorizeSpellInSlotIfNeeded(spell, slot)
     if slot <= 8 and not isSpellMemorizedInSlot(spell, slot) then
         mq.cmd("/memspell " .. slot .. " \"" .. spell .. "\"")
-        --print("Memorizing " .. spell .. " in gem slot " .. slot)
+        mq.delay(100)
+		print("Memorizing " .. spell .. " in gem slot " .. slot)
 		-- Wait until the spell is fully memorized and ready to cast
         while not isSpellReadyInSlot(spell, slot) do
 			--print("Checking" .. spell .. "--" .. slot)
