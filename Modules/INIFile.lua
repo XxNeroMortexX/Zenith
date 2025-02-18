cade1830b4ea213d2f9a315909284093d146aec1
XxNeroMortexX <XxNeroMortexX@gmail.com>
2025-02-17 17:23:22 -0700

    AutoCommit: Mon [ 02/17/2025 Time: 17:23:22 ]

--- Modules/INIFile.lua
+++ Modules/INIFile.lua
@@ -372,7 +372,7 @@ function readIniKeys(FindStrings, ReplaceWith)
             if type(key) == "string" and type(value) == "string" then
 				Debug("Creating Global Variable [ " .. key:lower() .. " = " .. value:lower() .. " ]")
 				ini[key:lower()] = value:lower() --this creates print(ini.key)
-				--print("Key:" .. key .. " - " .. "Value:" .. value)
+				print("Key:" .. key .. " - " .. "Value:" .. value)
 			else
 				-- Convert key and value to strings, handling tables appropriately
 				local keyStr = type(key) == "table" and tableToString(key) or tostring(key)
