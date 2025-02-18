3c89b383aec60a461c920df492ce49975721cf31
XxNeroMortexX <XxNeroMortexX@gmail.com>
2025-02-17 17:03:15 -0700

    AutoCommit: Mon [ 02/17/2025 Time: 17:03:15 ]

--- init.lua
+++ init.lua
@@ -144,7 +144,7 @@ local default_config = {
     }
 }
 
-local Function MemorizeSpells
+local Function MemorizeSpells()
 	-- Function to check if a spell is memorized in a specific gem slot
 	local function isSpellMemorizedInSlot(spell, slot)
 		--printf("Checking %s = %s",mq.TLO.Me.Gem(slot).Name(), spell)
