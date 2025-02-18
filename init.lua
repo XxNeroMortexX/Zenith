aaecba09bd35e2464876dcafa4815ba88b8409f0
XxNeroMortexX <XxNeroMortexX@gmail.com>
2025-02-17 17:28:37 -0700

    AutoCommit: Mon [ 02/17/2025 Time: 17:28:37 ]

--- init.lua
+++ init.lua
@@ -213,7 +213,7 @@ if ini then
     else
         print("The ini table is empty or not initialized.")
     end
-print(ini[debugs])
+print(ini[Debugs])
 --MemorizeSpells()
 
 -- Update the INI file with the default configuration
