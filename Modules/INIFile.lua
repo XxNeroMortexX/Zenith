bf6d15e36787923864132429d786999bbfc83042
XxNeroMortexX <XxNeroMortexX@gmail.com>
2025-02-17 18:11:31 -0700

    AutoCommit: Mon [ 02/17/2025 Time: 18:11:31 ]

--- Modules/INIFile.lua
+++ Modules/INIFile.lua
@@ -358,7 +358,14 @@ function readIniKeys(FindStrings, ReplaceWith)
         file:close()
     end
     -- Create a table to store the variables
+    --ini = {}
+	-- Create a table to store the variables with a metatable
     ini = {}
+    setmetatable(ini, {
+        __index = function(t, k)
+            return rawget(t, k:lower())
+        end
+    })
     
 	
     for section, keys in pairs(iniData) do
